3c1d08703b6b9be0c7276788ccc91e67b8eaf53a
lapshuntanya <lapshun.tatiana@gmail.com>
2024-04-25 21:51:27 +0300

    RAM >= 10 Gb

--- Lesson-18-Task1/Lesson-18-Task1.cpp
+++ Lesson-18-Task1/Lesson-18-Task1.cpp
@@ -16,6 +16,18 @@ int main()
     {
         arr[i].showInfo();
     }
+
+    cout << "=====================================\n\n";
+    int amount = 0;
+    for (int i = 0; i < N; i++)
+    {
+        if (arr[i].RAM >= 10) {
+            arr[i].showInfo();
+            amount++;
+        }
+    }
+    cout << "Amount: " << amount << endl;
+
 }
 
 
